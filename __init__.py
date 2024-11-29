e8da45644529256866d5f27ecb24c28ffc2efd5e
chenpipi0807 <144689950+chenpipi0807@users.noreply.github.com>
2024-11-29 11:19:34 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,14 @@
 from .PIP_Class import PIPClass
+from .PIP_Class_Advanced import PIPClassAdvanced
 
 NODE_CLASS_MAPPINGS = {
-    "PIPClass": PIPClass
+    "PIPClass": PIPClass,
+    "PIPClassAdvanced": PIPClassAdvanced
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "PIPClass": "PIP 图像分类"
+    "PIPClass": "PIP 图像分类",
+    "PIPClassAdvanced": "PIP 图像分类高级版"
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS'] 
